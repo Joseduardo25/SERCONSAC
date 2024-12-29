8bc2fa5b7b16c2bf778bf7c10e942a2820690f36
Eduardo Calachua <jcalachua@dtransforma.com>
2024-12-28 19:28:40 -0500

    add: section service one

--- src/components/ControlBilling/ControlBilling.tsx
+++ src/components/ControlBilling/ControlBilling.tsx
@@ -5,16 +5,25 @@ import { Reveal } from '../Reveal'
 
 export const ControlBilling = () => {
   return (
-    <div className='p-4 py-20 md:py-64'>
+    <div className='p-4 py-20 md:pt-64 md:pb5'>
+      <div className='mx-auto flex justify-center mb-8'>
+        <Reveal>
+          <h2 className='text-5xl font-semibold'>
+            PRINCIPALES SERVICIOS
+          </h2>
+        </Reveal>
+      </div>
       <div className='flex flex-col-reverse max-w-5xl mx-auto md:flex-row place-content-evenly'>
-        <MotionTransition className='flex-1 p-6 md:p-8'>
+        {/* <MotionTransition className='flex-1 p-6 md:p-8'>
           <Image src='/assets/control-business.png' width={580} height={411} alt='Control billing' className='w-full h-auto'/>
-        </MotionTransition>
+        </MotionTransition> */}
         <div className='flex-1 p-6 md:p-8'>
           <Reveal>
-            <h2 className='text-5xl font-semibold'>
-              Controla fácilmente
-              <span className='block'>tu facturación</span>
+            <h2 className='text-3xl font-semibold'>
+              IPM SISTEMAS CONSTRA 
+              <span className='block'>
+                INCENDIOS-NFPA 25
+              </span>
             </h2>
           </Reveal>
           <Reveal>
@@ -25,6 +34,9 @@ export const ControlBilling = () => {
             </p>
           </Reveal>
         </div>
+        <MotionTransition className='flex-1 p-6 md:p-8'>
+          <Image src='/assets/ipm-system.png' width={580} height={411} alt='Control billing' className='w-full h-auto'/>
+        </MotionTransition>
       </div>
     </div>
   )
