a958cbc0448f87297ce808545be81a433070dc79
Eduardo Calachua <jcalachua@dtransforma.com>
2025-01-04 01:19:38 -0500

    add: props to whatsapp button

--- src/components/WhatsappButton/WhatsappButton.jsx
+++ src/components/WhatsappButton/WhatsappButton.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 export function WhatsappButton() {
-
+  const chatMessage = (
+    <>
+      ¡Hola!🤝<br />
+      ¿Cómo podemos ayudarte?
+    </>
+  );
   return (
-      <FloatingWhatsApp className='text-black' phoneNumber='+51928133695' />
+      <FloatingWhatsApp 
+        className='text-black' 
+        phoneNumber='+51928133695'
+        accountName='Sofia'
+        statusMessage='Generalmente responde en 1 hora'
+        chatMessage={chatMessage}
+      />
   )
 }
