1074f83f43c1049891349bd4101c602607626f2c
Eduardo Calachua <jcalachua@dtransforma.com>
2025-01-05 19:27:02 -0500

    add: new high quality customers

--- src/components/PaymentsMethods/Customers.data.ts
+++ src/components/PaymentsMethods/Customers.data.ts
@@ -1,58 +1,58 @@
 export const CustomerData = [
   {
     id: 1,
-    image: "customer-1.png",
+    image: "customer-01-v2.jpeg",
   },
   {
     id: 2,
-    image: "customer-2.png",
+    image: "customer-02-v2.jpeg",
   },
   {
     id: 3,
-    image: "customer-3.png",
+    image: "customer-03-v2.jpeg",
   },
   {
     id: 4,
-    image: "customer-4.png",
+    image: "customer-04-v2.jpeg",
   },
   {
     id: 5,
-    image: "customer-5.png",
+    image: "customer-05-v2.jpeg",
   },
   {
     id: 6,
-    image: "customer-6.png",
+    image: "customer-06-v2.jpeg",
   },
   {
     id: 7,
-    image: "customer-7.png",
+    image: "customer-07-v2.jpeg",
   },
   {
     id: 8,
-    image: "customer-8.png",
+    image: "customer-08-v2.jpeg",
   },
   {
     id: 9,
-    image: "customer-9.png",
+    image: "customer-09-v2.jpeg",
   },
   {
     id: 10,
-    image: "customer-10.png",
+    image: "customer-10-v2.jpeg",
   },
+  // {
+  //   id: 11,
+  //   image: "customer-11.jpeg",
+  // },
+  // {
+  //   id: 12,
+  //   image: "customer-12.jpeg",
+  // },
   {
-    id: 11,
-    image: "customer-11.png",
-  },
-  {
-    id: 12,
-    image: "customer-12.png",
+    id: 1,
+    image: "customer-01-v2.jpeg",
   },
   {
     id: 1,
-    image: "customer-1.png",
-  },
-  {
-    id: 1,
-    image: "customer-2.png",
+    image: "customer-02-v2.jpeg",
   },
 ];
