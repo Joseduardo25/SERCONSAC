b05721a8474f833d8d0eca61b46ae1b380f0317e
Eduardo Calachua <jcalachua@dtransforma.com>
2025-01-05 19:10:43 -0500

    add: text white

--- src/components/CtaDark/CtaDark.tsx
+++ src/components/CtaDark/CtaDark.tsx
@@ -6,7 +6,7 @@ export const CtaDark = () => {
   return (
     <div className='px-6 my-20 md:mt-36'>
       <div className='max-w-5xl px-4 py-6 mx-auto border-transparent bg-radialBlack md:px-16 md:py-12 shadow-dark rounded-3xl'>
-        <div className='grid items-center md:grid-cols-2'>
+        <div className='grid items-center text-white md:grid-cols-2'>
           <div className='my-8'>
             <Reveal>
               <h3 className='text-3xl font-bold'>Ven a probar nuestros servicios</h3>
