a5f19fe65442a6434ca5685a00432b3bec79aaf8
Eduardo Calachua <jcalachua@dtransforma.com>
2025-01-05 20:41:42 -0500

    add: video

--- src/components/CtaDark/CtaDark.tsx
+++ src/components/CtaDark/CtaDark.tsx
@@ -6,20 +6,25 @@ export const CtaDark = () => {
   return (
     <div className='px-6 my-20 md:mt-36'>
       <div className='max-w-5xl px-4 py-6 mx-auto border-transparent bg-radialBlack md:px-16 md:py-12 shadow-dark rounded-3xl'>
-        <div className='grid items-center text-white md:grid-cols-2'>
+        <div className='grid items-center text-white md:grid-cols-1'>
           <div className='my-8'>
             <Reveal>
-              <h3 className='text-3xl font-bold'>Ven a probar nuestros servicios</h3>
+              <h3 className='text-3xl font-bold'>Pruebas de Aceptación de SCI - Unidades de Proceso y Auxiliares de la Nueva Refinería Talara</h3>
             </Reveal>
             <Reveal>
-              <p>Todo lo que necesitas para aceptar pagos con tarjeta y haga crecer su negocio en cualquier parte del planeta</p>
+              <p className='md:mt-6'>
+                Realizamos de forma satisfactoria las pruebas de aceptación de SCI de todas las unidades de proceso y 
+                auxiliares de la nueva refinería de Talara. Gracias a nuestro cliente Petro Perú por confiar en nuestro trabajo.
+              </p>
             </Reveal>
           </div>
-          <Link href='#' className='px-4 py-3  mx-auto rounded-md bg-blueRadial'>
+          {/* <Link href='#' className='px-4 py-3  mx-auto rounded-md bg-blueRadial'> */}
+          <div className='mx-auto md: py-8'>
             <Reveal>
-              Empieza Ahora
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/VlbH2mQhdcM?si=dwD4mWY3FQE9f6GK" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </Reveal>
-          </Link>
+          </div>
+          {/* </Link> */}
         </div>
       </div>
     </div>
